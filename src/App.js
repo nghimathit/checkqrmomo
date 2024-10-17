a794d820cbf5f44485a3491115a60528c6e99cf7
nghimathit <vannghinguyen152001@gmail.com>
2024-10-18 00:10:29 +0700

    fixNghi

--- src/App.js
+++ src/App.js
@@ -1,118 +1,130 @@
 import axios from "axios";
 import React, { useState } from "react";
-
 function App() {
   const [phoneNumbers, setPhoneNumbers] = useState([]);
   const [password] = useState("vanhuY90$");
   const [newPhoneNumbers, setNewPhoneNumbers] = useState("");
   const [AllAmount, setAllAmount] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [ActiveDay, setActiveDay] = useState("Now");
   const today = new Date();
   const [day, setDay] = useState(String(today.getDate()).padStart(2, "0"));
-  const [month, setMonth] = useState(String(today.getMonth() + 1).padStart(2, "0"));
-  const [year, setYear] = useState(today.getFullYear());
-
+  const [month] = useState(String(today.getMonth() + 1).padStart(2, "0"));
+  const [year] = useState(today.getFullYear());
+  const [error, seteError] = useState();
   const formattedDate = `${year}-${month}-${day}`;
-  console.log("Today:", formattedDate);
-
   const loginAndGetAmount = async (username) => {
-    const getmerchant =
-      "/api/profile/v2/merchants?requestType=LOGIN_MERCHANTS&language=vi";
-    const headers = {
-      // Các header cần thiết nếu có
-    };
-
+    const getmerchant = "/api/profile/v2/merchants?requestType=LOGIN_MERCHANTS&language=vi";
     const data = {
       username: username,
       password: password,
     };
-
     try {
       const response = await axios.post(
         "/api/authentication/login?language=vi",
         data,
-        {
-          headers: headers,
-        }
       );
-
       const token = response.data.data.token;
-
       const merchantResponse = await axios.get(getmerchant, {
         headers: {
           Authorization: "Bearer " + token,
         },
       });
-
       const merchantId = merchantResponse.data.data.merchantResponseList[0].id;
       const brandName = merchantResponse.data.data.merchantResponseList[0].brandName;
-
       const transactionData = await axios.get(
-        `api/transaction/v2/transactions/statistics?pageSize=10&pageNumber=0&fromDate=${formattedDate}T00%3A00%3A00.00&toDate=${formattedDate}T23%3A59%3A59.00&status=ALL&merchantId=${merchantId}&language=vi`,
+        `api/transaction/v2/transactions/statistics?pageSize=10&pageNumber=0&fromDate=${ActiveDay === "AllDay"?firstDay:formattedDate}T00%3A00%3A00.00&toDate=${ActiveDay === "AllDay"?lastDay:formattedDate}T23%3A59%3A59.00&status=ALL&merchantId=${merchantId}&language=vi`,
         {
           headers: {
             Authorization: "Bearer " + token,
           },
         }
       );
-
       const totalSuccessAmount = transactionData?.data?.data?.totalSuccessAmount || 0;
-
-      // Trả về cả amount và brandName
       return { amount: totalSuccessAmount, brandName };
     } catch (error) {
-      console.error("Error during login or data fetching:", error);
+      seteError("Đã Có Lỗi, Cần Chạy Lại... Kiểm Tra Các SĐT phải cùng 1 Mật Khẩu Nhé. Thực Hiện Lại Sau 5s");
+      console.error("Lỗi Nghi Ơi:", error);
       return { amount: 0, brandName: "" };
+     
     }
   };
-
-  const loginAllPhones = () => {
+  const loginAllPhones = async () => {
     setAllAmount([]);
     setTotalAmount(0);
-    phoneNumbers.forEach(async (phone) => {
+    seteError();
+    for (const phone of phoneNumbers) {
       const { amount, brandName } = await loginAndGetAmount(phone);
-
-      // Cập nhật từng kết quả vào state và tính tổng tiền
       setAllAmount((prevAmounts) => [
         ...prevAmounts,
         { phone, amount, brandName },
       ]);
       setTotalAmount((prevTotal) => prevTotal + amount);
-    });
+    }
   };
-
   const handleAddPhoneNumbers = () => {
-    const phonesArray = newPhoneNumbers
-      .split(/[,\s]+/)
-      .filter((phone) => phone.trim() !== "");
-
+    const phonesArray = newPhoneNumbers.split(/[,\s]+/).filter((phone) => phone.trim() !== "");
     setPhoneNumbers([...phoneNumbers, ...phonesArray]);
     setNewPhoneNumbers("");
   };
-
   const HandleTotalYesterday = () => {
+    setActiveDay("Yesterday");
     setDay(String(today.getDate() - 1).padStart(2, "0"));
   };
-
   const HandleTotalNow = () => {
+    setActiveDay("Now");
     setDay(String(today.getDate()).padStart(2, "0"));
   };
+  const HandleTotalAllDayMonth =()=>{
+    setActiveDay("AllDay");
+  }
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+function getFirstAndLastDayOfMonth(year, month) {
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    return { firstDay: formatDate(firstDay), lastDay: formatDate(lastDay) };
+}
+
+const { firstDay, lastDay } = getFirstAndLastDayOfMonth(2024, month-1);
+
+
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 bg-gray-200/50 relative ">
       <h1 className="text-2xl font-bold mb-4 text-center">
-        Tổng Tiền Ngày: {formattedDate}
+        Tổng Tiền Ngày: {ActiveDay === "AllDay" ? `Toàn bộ tháng ${month}`:`${day}-${month}-${year}`}
       </h1>
-      <button className="block hover:bg-slate-400 mb-5 px-2 py-1 rounded-lg" onClick={HandleTotalNow}>Tổng Hôm Nay</button>
-      <button className="block hover:bg-slate-400 mb-5 px-2 py-1 rounded-lg" onClick={HandleTotalYesterday}>Tổng Hôm Qua</button> 
-
+      <button
+        className={`block hover:bg-slate-400 mb-5 px-2 py-1 rounded-lg ${ActiveDay === "Now" ? " bg-green-300" : ""}`}
+        onClick={HandleTotalNow}
+      >
+        Tổng Hôm Nay: <span className="font-bold text-red-500">{String(today.getDate()).padStart(2, "0")}/{String(today.getMonth() + 1).padStart(2, "0")}</span>
+      </button>
+      <button
+        className={`block hover:bg-slate-400 mb-5 px-2 py-1 rounded-lg ${ActiveDay === "Yesterday" ? " bg-green-300" : ""}`}
+        onClick={HandleTotalYesterday}
+      >
+        Tổng Hôm Qua :<span className="font-bold text-red-500">{String(today.getDate() - 1).padStart(2, "0")}/{String(today.getMonth() + 1).padStart(2, "0")}</span>
+      </button>
+      <button
+        className={`block hover:bg-slate-400 mb-5 px-2 py-1 rounded-lg ${ActiveDay === "AllDay" ? " bg-green-300" : ""}`}
+        onClick={HandleTotalAllDayMonth}
+      >
+        Tổng 1 Tháng :<span className="font-bold text-red-500">{`${firstDay}-${lastDay}`}</span>
+      </button>
       <div className="flex items-center justify-center mb-4">
         <textarea
           value={newPhoneNumbers}
           onChange={(e) => setNewPhoneNumbers(e.target.value)}
           placeholder="Nhập dãy số điện thoại, ngăn cách bằng dấu cách hoặc dấu phẩy"
-          className="border border-gray-300 rounded p-2 mr-2 w-32"
-          rows="10"
+          className="border border-gray-300 rounded p-2 mr-2 w-30"
+          rows="4"
         />
         <button
           onClick={handleAddPhoneNumbers}
@@ -121,7 +133,6 @@ function App() {
           Thêm số điện thoại
         </button>
       </div>
-
       <div className="flex items-center justify-center mb-6">
         <button
           onClick={loginAllPhones}
@@ -130,34 +141,37 @@ function App() {
           Đăng nhập và lấy số dư
         </button>
       </div>
-
       <div className="text-center">
-  <table className="table-auto w-full border-collapse border border-gray-400">
-    <thead>
-      <tr className="bg-gray-200">
-        <th className="border border-gray-300 px-4 py-2">Số điện thoại</th>
-        <th className="border border-gray-300 px-4 py-2">Tên Shop</th>
-        <th className="border border-gray-300 px-4 py-2">Số tiền</th>
-      </tr>
-    </thead>
-    <tbody>
-      {AllAmount.map((item, index) => (
-        <tr key={index} className="bg-white border-b">
-          <td className="border border-gray-300 px-4 py-2">{item.phone}</td>
-          <td className="border border-gray-300 px-4 py-2">{item.brandName|| "NULL"}</td>
-          <td className="border border-gray-300 px-4 py-2">
-           {item.amount.toLocaleString()} VND
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-
-
-      <h2 className="text-xl font-bold text-center mt-6">
-    Total {formattedDate}:  <span className="text-red-500"> {totalAmount.toLocaleString()} </span> VND
+        <table className="table-auto w-full border-collapse border border-gray-400">
+          <thead className="sticky top-0 right-0">
+            <tr className="bg-gray-200">
+              <th className="border border-gray-300 px-4 py-2">Số điện thoại</th>
+              <th className="border border-gray-300 px-4 py-2">Tên Shop</th>
+              <th className="border border-gray-300 px-4 py-2">Số tiền</th>
+            </tr>
+          </thead>
+          <tbody>
+            {AllAmount.map((item, index) => (
+              <tr key={index} className="bg-white border-b">
+                <td className="border border-gray-300 px-4 py-2">{item.phone}</td>
+                <td className="border border-gray-300 px-4 py-2">{item.brandName || "NULL"}</td>
+                <td className="border border-gray-300 px-4 py-2">{item.amount.toLocaleString()} VND</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="sticky bottom-0 right-0">
+        {error?
+        <h2 className="text-xl font-bold text-center mt-6 text-red-600 bg-yellow-400">{error}</h2>:
+       <>
+        <h2 className="text-xl font-bold text-center mt-6 bg-green-300 ">
+        Total {ActiveDay === "AllDay" ? `Toàn bộ tháng ${month}`:`${day}-${month}-${year}`}:{" "}
+        <span className="text-red-500"> {totalAmount.toLocaleString()} </span> VND
       </h2>
+       <p className=" bg-green-300"> <span className="text-red-600">{`${AllAmount.length} `}</span> QR</p></>
+      }
+      </div>
     </div>
   );
 }
---
+++ src/Loaing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loaing = () => {
+    return (
+        <div className='items-center w-full justify-center flex h-[100vh] bg-gray-200'>
+            <div className='w-8 h-8 border-4 border-black rounded-full border-t-white animate-spin mt-[-177px]'></div>
+        </div>
+    )
+}
+
+export default Loaing
